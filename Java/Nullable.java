277aeda1ba3f82ef483c4cdb2d0ff8edae289144
Louis Parent <louisparent66@yahoo.fr>
2020-10-06 15:34:17 +0200

    Add Nullable Java annotation

---
+++ Java/Nullable.java
@@ -0,0 +1,17 @@
+package fr.iut.utils.code;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.PARAMETER, ElementType.METHOD, ElementType.FIELD, ElementType.LOCAL_VARIABLE})
+@Documented
+@Inherited
+public @interface Nullable
+{
+
+}
