e5fd2ed53c5d03deff23e6eea9b9170062dd7833
Appixroid <appixroid@gmail.com>
2020-10-07 00:23:05 +0200

    Removing unused javascript lib

--- Javascript/Selectable/selectable.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("load", function() {
-	document.querySelectorAll("[selectable=false]").forEach(function(elem){
-		elem.style += "-webkit-touch-callout: none;-webkit-user-select: none;-khtml-user-select: none;-moz-user-select: none;-ms-user-select: none;user-select: none;";
-	});
-});
